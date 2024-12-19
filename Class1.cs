15b1120a70b111751bab4e57e1c25e724a494542
Mike <grimaldo.mike@hotmail.com>
2024-12-18 18:07:14 -0600

    Release WebGL a itchio v0.05.21
    Push con la version webGL a la página de itch.io de la version 0.05.21:
    https://officelinebacker.itch.io/animal-strategy

---
+++ Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public class Class1
+{
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+s
+
+
+class Result
+{
+
+    /*
+     * Complete the 'minimalHeaviestSetA' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<int> minimalHeaviestSetA(List<int> arr)
+    {
+        //Checking if the array is even or odd
+        int arrCount = arr.Count();
+        int sizeOfSetA = 0;
+        if (arrCount % 2 == 0)
+        {
+            arrCount = arrCount / 2 - 1;
+        }
+        else
+        {
+
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = new List<int>();
+
+        for (int i = 0; i < arrCount; i++)
+        {
+            int arrItem = Convert.ToInt32(Console.ReadLine().Trim());
+            arr.Add(arrItem);
+        }
+
+        List<int> result = Result.minimalHeaviestSetA(arr);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
+}
